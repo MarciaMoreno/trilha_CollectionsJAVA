8d13f29d45e73408735d44ff31f2e02a81abfae1
Marcia P. Moreno <mapat.soad@gmail.com>
2024-07-05 22:38:22 -0300

    Introdução ao Collections JAVA - Lista de Tarefas ou Array List

---
+++ GenericListExemplos/src/exemploDeGenericList/GenericListExemplo1.java
@@ -0,0 +1,31 @@
+package exemploDeGenericList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericListExemplo1 {
+	
+		public static void main(String[] args) {
+			// Exemplo sem Generics
+			List listaSemGenerics = new ArrayList();
+			listaSemGenerics.add("Elemento 1");
+			listaSemGenerics.add(10); // Permite adicionar qualquer tipo de objeto
+
+			// Exemplo com Generics
+			List<String> listaGenerics = new ArrayList<>();
+			listaGenerics.add("Elemento 1");
+			listaGenerics.add("Elemento 2");
+
+			// Iterando sobre a lista com Generics
+			for (String elemento : listaGenerics) {
+				System.out.println(elemento);
+			}
+
+			// Iterando sobre a lista sem Generics (necessário fazer cast)
+			for (Object elemento : listaSemGenerics) {
+				String str = (String) elemento;
+				System.out.println(str);
+			
+		}
+	}
+}
---
+++ OperacoesBasicasComList/src/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,19 @@
+package OperacoesBasicas;
+
+public class Tarefa {
+
+	private String descricao;
+	
+	public Tarefa(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	@Override
+	public String toString() {
+		return descricao;
+	}
+		
+}
